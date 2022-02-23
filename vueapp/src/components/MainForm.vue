e5efff3aaa92a523d771bde5f1614848d1f6be09
Roger <rogerchapa68@gmail.com>
2022-02-22 19:57:55 -0600

    work so far
    for loop going, input and submit button on there

--- vueapp/src/components/MainForm.vue
+++ vueapp/src/components/MainForm.vue
@@ -1,12 +1,29 @@
 <template>
   <v-container>
     <!-- use this piece to take in an input from the user, once the put in an input, show a good looking form that generates the number the user requests -->
+    <v-card-title>Number of Participants:      
+        <input type="number"
+        min=3
+        max=50
+        v-model="count"
+        @input="validateCount">
+            
+        <v-btn color="primary" :disabled="disableBtn" v-on:click="submit">Submit</v-btn>
+    </v-card-title>
+
     <v-card-title>Participant Forms</v-card-title>
     <v-card-subtitle>
       Please enter each participant's name, email, and a short description of
       them below:
     </v-card-subtitle>
-    <ParticipantForm />
+
+    <ul>
+        <li v-for="index in parseInt(count)" :key="index">
+            Participant #{{index}} <ParticipantForm/>
+        </li>
+    </ul>
+    
+    
     <div v-if="valid">
       <SantaSetup @failure="error" :value="participantData" />
     </div>
@@ -27,13 +44,13 @@ export default {
       //you will need to set this up, it should take in an input in html from the user
       //to get the total count, you will use this count to generate the form with a v-data-table or some sort of thing
       //to create the full form, use vuetify!
-      count: 0,
-      inputRule:
-        "change this to where the input gets validated, it must be between 3 and 50! look into this on vue",
+      count: 3,
+      inputRules: {}, // I just ended up using a method validateNumber and greyed out the button? hope thats otay
       valid: true, //this will be an object bound to the table/form created to pass in to the santaSetup, they shoudl be validated from the participant form
       //this will be filled dynamically
       participantData: [{ name: "", email: "", about: "" }],
       problem: false,
+      disableBtn: false
     };
   },
   components: {
@@ -44,8 +61,10 @@ export default {
   methods: {
     submit() {
       //this can be wehre you validate on submission or on form button click to generate full form,
-      //this will pass the data placed into the participant data object into the santaSetup modal, pass it as a prop, you won't need to show the modal, unless you decide to, but the modal
+      //this will pass the data placed into the participant data object into the santaSetup
+      //modal, pass it as a prop, you won't need to show the modal, unless you decide to, but the modal
       //itself will be a "Generating..." while the algorithm happens behinds the scenes
+      console.log("oh yea you like that")
     },
     add() {
       //another method you could use to add each form to your participant data object by pushing it, you are pushing an object to an array
@@ -54,6 +73,30 @@ export default {
       console.log("in error");
       this.problem = true;
     },
+
+    validateCount () {
+        if(this.count > 50 || this.count < 3) {
+            this.disableBtn = true
+        }
+        else{
+            this.disableBtn = false
+        }
+    }
   },
 };
 </script>
+
+<style scoped>
+input {
+  border: 1px solid grey;
+  border-radius: 5px;
+  height: 25px;
+  margin-left: 10px;
+  margin-right: 20px;
+  text-align: center;
+}
+
+input[type=number]::-webkit-inner-spin-button {
+    opacity: 1
+}
+</style>
